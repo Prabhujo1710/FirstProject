13c4ec3ca2ebe1dd316007ff1b6c7be75bb733b1
Prabhujo1710 <prabhujo1710@gmail.com>
2023-01-21 20:36:43 +0530

    My First git

---
+++ src/test/java/org/com/Myfiles.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class Myfiles {
+
+	private void Company() {
+
+		System.out.println("Greens");
+	}
+	
+	private void Employee() {
+	
+		System.out.println("Prabhu");
+	}
+	private void Client() {
+		System.out.println("Praveen");
+	
+	}
+	
+}
